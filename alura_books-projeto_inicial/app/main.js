3b70c719ce2319fee4ddc67992b2c69bfbd00929
Matheus Souza <108445533+theusito@users.noreply.github.com>
2023-01-10 17:49:46 -0300

    Add files via upload

---
+++ alura_books-projeto_inicial/app/main.js
@@ -0,0 +1,10 @@
+let livros = []
+const endpoitDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
+
+async function getBuscarLivrosDaAPI(){
+    const res = await fetch(endpoitDaAPI)
+    livros = await res.json()
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
+}
